1d1af8bb589718222e3024abbf2ee97890051caf
Alvaro Capaceta <ingcapadev@gmail.com>
2023-08-10 17:34:16 -0700

    feat: Update countdown through timepicker

--- src/pages/config/Config.jsx
+++ src/pages/config/Config.jsx
@@ -4,21 +4,45 @@ import { TimePicker } from 'react-ios-time-picker';
 
 const Config = () => {
   const { updateValues } = useCountdownContext();
-  const [time, setTime] = useState('10:00');
+  const [time, setTime] = useState();
+  const [minutes, setMinutes] = useState();
+  const [hours, setHours] = useState();
+  const [isDisabled, setIsDisabled] = useState(true);
 
   const onChange = (timeValue) => {
     setTime(timeValue);
+    setIsDisabled(false);
+    const [hoursValue, minutesValue] = timeValue.split(':');
+    setMinutes(parseInt(minutesValue));
+    setHours(parseInt(hoursValue));
+  };
+
+  const onUpdateTime = () => {
+    updateValues(hours, minutes, 0);
   };
 
   return (
     <>
+      <h2 className='text-center text-2xl font-bold'>Configuración</h2>
+      <p className='text-center text-lg'>
+        ¿Con qué frecuencia come tu mascota?
+      </p>
+
       <TimePicker
         onChange={onChange}
         value={time}
         cellHeight={40}
         cancelButtonText='Cancelar'
         saveButtonText='Guardar'
+        placeHolder='Tiempo'
       />
+
+      <button
+        className='btn btn-primary'
+        disabled={isDisabled}
+        onClick={onUpdateTime}>
+        Actualizar
+      </button>
     </>
   );
 };
